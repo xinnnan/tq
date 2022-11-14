df15ec7312ae390322e3251c6c9fa1c778613f80
xinnan cao <xinnancao@gmail.com>
2022-11-13 23:00:12 -0500

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,14 @@ $(document).ready(function(){
       $('#last').hide();
     }
     }
+    function adding_ques(data,i){
+      $('#question').text(data[i].Quiz)
+				$('#optionsA').text(data[i].option1)
+				$('#optionsB').text(data[i].option2)
+				$('#optionsC').text(data[i].option3)
+				$('#optionsD').text(data[i].option4)
+				$('#number').text(Number(i + 1));
+    }
 
 
 
@@ -54,15 +62,32 @@ $(document).ready(function(){
 
 
 
-// questions options and answers array, will try use json
-
-fetch('qoa.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// questions options and answers array
+data = {
+  "qoa":[
+      {"question":"what is bootstrap?",
+      "optiona":"A. Bootstrap is a powerful, feature-packed frontend toolkit.",
+      "optionb":"B. A programming language.",
+      "optionc":"C. A software.",
+      "optiond":"D. A hardware componenet.",
+      "answer":"A. Bootstrap is a powerful, feature-packed frontend toolkit."
+      },
+      {"question":"what is jquery?",
+      "optiona":"A.jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation.",
+      "optionb":"B. A programming language.",
+      "optionc":"C. A software.",
+      "optiond":"D. A hardware componenet.",
+      "answer":"A.jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation."
+      },
+      {"question":"what is html?",
+      "optiona":"A. A software framework.",
+      "optionb":"B. A programming language.",
+      "optionc":"C. A software.",
+      "optiond":"D. A hardware componenet.",
+      "answer":"B. A programming language."
+      }
+  ]
+};
 
 
 
